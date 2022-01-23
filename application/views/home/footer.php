e8af5af6e52a245fac726feff69adb46048fb7ae
damaputraa <dama.rahmad21@gmail.com>
2022-01-24 00:05:05 +0700

    first commit

---
+++ application/views/home/footer.php
@@ -0,0 +1,86 @@
+<!-- ============== Footer Section ============== -->
+        <footer>
+            <div class="container about-section">
+                <!--
+                <div class="row relative">
+                    <div class="news-letter">
+                        <h2>News Letter</h2>
+                        <p>Enter your email address for getting updates</p>
+                        <div class="subscribe-box">
+                            <div class="col-sm-8 col-xs-8 less-right">
+                                <input type="text" placeholder="Email Address" class="news-letter-box" />
+                            </div>
+                            <div class="col-sm-4 col-xs-4 less-left">
+                                <button type="button" class="news-btn hvr-bounce-to-right-orange">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                -->
+                <div class="row">
+                    <div class="footer-links">
+                        <div class="col-lg-8 col-md-8 col-sm-8 footer-logo">
+                            <img src="<?php echo base_url('gambar/logo-biru.png') ?>" width="250px" height="100px">
+                            <div id="consulty">
+                                <div class="media-body2"><strong>SIG CV. ROSIDA JAYA</strong></div>
+                            </div>
+                        </div>
+                        <div class="clearfix visible-xs"></div>
+                        <div class="col-md-4">
+                            <h2>Kontak</h2>
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <ul id="schort1">
+                                        <li><span>Telepon :</span>
+                                            <a href="#"> 082124772867</a>
+                                        </li>
+                                        <li><span>Email :</span>
+                                            <a href="#">dama.rahmad21@gmail.com</a>
+                                        </li>
+                                        <li class="social-media">
+                                            <a href="https://facebook.com/"><i class="fa fa-facebook fa-x3"></i></a>
+                                            <a href="https://twitter.com/"><i class="fa fa-twitter fa-x3"></i></a>
+                                            <a href="https://www.instagram.com/"><i class="fa fa-instagram fa-x3"></i></a>
+                                            <a href="https://plus.google.com/"><i class="fa fa-google-plus fa-x3"></i></a>
+                                            <a href="https://www.rss.com/"><i class="fa fa-rss fa-x3"></i></a>
+                                            <a href="https://www.youtube.com/"><i class="fa fa-youtube fa-x3"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.col-lg-2 -->
+                    </div>
+                    <!-- /.footer-links -->
+                </div>
+            </div>
+            <!-- /.container -->
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <p class="footer-text-margin">2022 &copy; Rachmad Ananda Damaputra. All rights Reserved | Repost by <a href='https://damadev.my.id/' title='damadev.my.id/' target='_blank'>damadev.my.id/</a>
+                        </p>
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container -->
+            </div>
+            <!-- /.footer-bottom -->
+        </footer>
+    </div>
+    <!-- Back to top button -->
+    <a href="#" id="scroll" title="Scroll to Top">Top<span></span></a>
+    <!-- /.uv-rounds-wrapping -->
+    <!-- JavaScripts Starts here -->
+
+    <script src="<?php echo base_url()?>assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url()?>assets/vendors/bxslider/js/jquery.bxslider.min.js"></script>
+    <!--revolution-slider-->
+    <script src="<?php echo base_url()?>assets/vendors/revolution-slider/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendors/revolution-slider/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendors/OwlCarousel/owl-carousel/owl.carousel.js"></script>
+    <script src="<?php echo base_url()?>assets/vendors/wow/wow.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/custom.js"></script>
+    <script src="<?php echo base_url()?>assets/js/index.js"></script>
+</body>
+</html>
---
+++ application/views/home/v_contact.php
@@ -0,0 +1,57 @@
+<br><br><br><br><br><br>
+<div class="container" id="contact5">
+            <div class="row">
+                <div class="col-xs-12 col-md-12 col-lg-12">
+                    <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.2s" id="contact"><strong>DETAIL KONTAK</strong></h3>
+                    <div class="row wow zoomInDown" data-wow-duration="0.5s" data-wow-delay="0.2s">
+                        <div class="col-xs-1 col-md-1 col-lg-1">
+                            <i class="fa fa-map-marker contact_details"></i>
+                        </div>
+                        <div class="col-xs-10 col-md-10 col-lg-10">
+                            <h4 class="par1"><strong>ALAMAT</strong></h4>
+                            <p class="par1">JL. Patin IV nomor 4
+                                <br/> Kota Palangka Raya
+                                <br/> Kalimantan Tengah , Indonesia</p>
+                        </div>
+                    </div>
+                    <div class="row wow zoomInDown" data-wow-duration="0.5s" data-wow-delay="0.4s">
+                        <div class="col-xs-1 col-md-1 col-lg-1">
+                            <i class="fa fa-phone contact_details"></i>
+                        </div>
+                        <div class="col-xs-10 col-md-10 col-lg-10">
+                            <h4 class="par1"><strong>HANDPHONE</strong></h4>
+                            <p class="par1">0821 2477 2867
+                                <br/>
+                                <a href="https://wa.me/6282124772867" target="_blank"> Whatsapp : 082124772867</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row  wow zoomInDown" data-wow-duration="0.5s" data-wow-delay="0.6s">
+                        <div class="col-xs-1 col-md-1 col-lg-1">
+                            <i class="fa fa-inbox contact_details"></i>
+                        </div>
+                        <div class="col-xs-10 col-md-10 col-lg-10">
+                            <h4 class="par1"><strong>EMAIL</strong></h4>
+                            <p class="par1">
+                                <a href="#"> dama.rahmad21@gmail.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row  wow zoomInDown" data-wow-duration="0.5s" data-wow-delay="0.8s">
+                        <div class="col-xs-1 col-md-1 col-lg-1">
+                            <i class="fa fa-share-alt contact_details"></i>
+                        </div>
+                        <div class="col-xs-10 col-md-10 col-lg-10">
+                            <h4 class="par1"><strong>SOCIAL MEDIA</strong></h4>
+                            <div class="link-box2">
+                                <a href="#"><i class="twitter fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-instagram twitter"></i></a>
+                                <a href="#"><i class="fa fa-google twitter">+</i></a>
+                                <a href="#"><i class="fa fa-youtube twitter"></i></a>
+                            </div>
+                            <!-- /.link-box2 -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
